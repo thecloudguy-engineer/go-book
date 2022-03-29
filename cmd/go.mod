5fc97bf3d68d09f5c757ac767f6b9c9f9778d35b
thecloudguy <thecloudguy.engineer@gmail.com>
2022-03-29 18:21:14 -0500

    fixed some minor errors

--- cmd/go.mod
+++ cmd/go.mod
@@ -1,3 +1,14 @@
 module main.go
 
 go 1.18
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/thecloudguy-engineer/go-book v0.0.0-20220329212025-503ffae9f9f3
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -10,6 +10,6 @@ var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("book/{bookId}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
